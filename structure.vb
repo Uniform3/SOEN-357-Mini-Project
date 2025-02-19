216b1fdd2bb323cb378448ec181a66928bfbb717
Tristan <tristansf@outlook.com>
2025-02-19 17:50:34 -0500

    initial commit with all working files

---
+++ structure.vb
@@ -0,0 +1,10 @@
+MediManage/
+├─ index.html                  (Home page)
+├─ medications.html            (Medications page)
+├─ add-medication.html         (Add New Medication page)
+├─ appointments.html           (Appointments page)
+├─ schedule-appointment.html   (Schedule New Appointment page)
+├─ messages.html               (Messages page)
+├─ new-message.html            (New Message page)
+├─ styles.css                  (Shared CSS)
+└─ scripts.js                  (Optional JavaScript)
